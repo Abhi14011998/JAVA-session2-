7b020aa5f362704bfaf4e823b16d17b8a6f215df
Abhi14011998 <abhinavsawantcool@gmail.com>
2016-04-27 01:47:42 +0530

    assignment1

---
+++ EligibleForVoteOrNot.java
@@ -0,0 +1,22 @@
+
+public class EligibleForVoteOrNot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int age = 7;
+		
+		if (age >= 18)
+		{
+			System.out.println("Person is eligible for voting.");
+		}
+		else 
+		{
+			System.out.println("Person is not eligible for voting.");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
